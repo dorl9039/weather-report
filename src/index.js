7aebdaf7c6865231dd5308982203f597965961c9
Airis Wang <airis.wang2014@fgvmail.br>
2023-06-07 17:02:49 -0400

    implemented temperature color change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const state = {
     increaseTempControl: null,
     decreaseTempControl: null,
@@ -14,14 +16,30 @@ const loadControls = () => {
 loadControls()
 
 // event handling
+const changeTempColor = () => {
+    if (state.temp >= 80) {
+        state.tempValue.setAttribute('class', 'red');
+    } else if (state.temp >= 70 && state.temp <= 79) {
+        state.tempValue.setAttribute('class', 'orange');
+    } else if (state.temp >= 60 && state.temp <= 69) {
+        state.tempValue.setAttribute('class', 'yellow');
+    } else if (state.temp >= 50 && state.temp <= 59) {
+        state.tempValue.setAttribute('class', 'green');
+    } else {
+        state.tempValue.setAttribute('class', 'teal');
+    }
+}
+
 const increaseTemp = () => {
     state.temp++;
     state.tempValue.textContent = state.temp;
+    changeTempColor();
 }
 
 const decreaseTemp = () => {
     state.temp--;
     state.tempValue.textContent = state.temp;
+    changeTempColor();
 }
 
 const registerEventHandler = () => {
